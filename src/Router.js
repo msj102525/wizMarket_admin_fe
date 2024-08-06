64f321adcbfc1641863c719a14055ebed2a87023
tpals213 <tpals213@gmail.com>
2024-08-06 11:37:27 +0900

    초기 세팅 커밋

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import Main2 from "./pages/Main2/Main2";
 import Main3 from "./pages/Main3/Main3";
+import Local from  './pages/Population/Local'
+import Map from './pages/Population/Map'
 
 const Router = () => {
   return (
@@ -11,6 +13,8 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/main2" element={<Main2 />} />
         <Route path="/main3" element={<Main3 />} />
+        <Route path="/local" element={<Local />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                     <Link to="/">Go to main</Link>
                 </li>
                 <li>
-                    <Link to="/main2">Go to main2</Link>
+                    <Link to="/map">지역 인구 분포도</Link>
                 </li>
                 <li>
                     <Link to="/main3">Go to main3</Link>
