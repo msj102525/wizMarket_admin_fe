5a65fd7731c343c7ecd29e3a8ab224fdd3194b2b
tpals213 <tpals213@gmail.com>
2024-09-12 16:38:47 +0900

    매장정보 지도연동, 업종 분류 표 리스트 시작

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -25,33 +25,79 @@ const Category = () => {
         setIsList(!isList);
     };
 
+
+
     return (
 
         <div>
             <Header />
             <div className="flex">
                 <Aside />
-                <main className="gap-2 pr-10 w-full">
+                <main className="flex-1 flex flex-col gap-2 min-h-screen p-4">
                     <section>
-                        <SectionHeader title="업종 분석" isList={isList} handleToggle={handleToggle} />
+                        <SectionHeader title="업종 분류" isList={isList} handleToggle={handleToggle} />
                     </section>
-                    <section>
-                        <div>
-                            <CategorySelect
-                                reference={reference}
-                                references={references}
-                                setReference={setReference}
-                                mainCategory={mainCategory}
-                                setMainCategory={setMainCategory}
-                                mainCategories={mainCategories}
-                                subCategory={subCategory}
-                                setSubCategory={setSubCategory}
-                                subCategories={subCategories}
-                                detailCategory={detailCategory}
-                                setDetailCategory={setDetailCategory}
-                                detailCategories={detailCategories}
-                            />
-                        </div>
+                    
+                    <section className="bg-white p-4">
+
+                        {/* 검색 부 */}
+                        <section className="flex items-center space-x-2 bg-gray-100 p-4 rounded-md">
+                            <div className='w-1/2'>
+                                <CategorySelect 
+                                    reference={reference} 
+                                    references={references} 
+                                    setReference={setReference}
+                                    mainCategory={mainCategory} 
+                                    setMainCategory={setMainCategory}
+                                    mainCategories={mainCategories}
+                                    subCategory={subCategory} 
+                                    setSubCategory={setSubCategory}
+                                    subCategories={subCategories}
+                                    detailCategory={detailCategory} 
+                                    setDetailCategory={setDetailCategory}
+                                    detailCategories={detailCategories}
+                                />
+                            </div>
+                            <div>
+                                <button className='ml-4 bg-black text-white px-4 py-2 rounded'>
+                                    검색
+                                </button>
+                            </div>
+                        </section>
+
+                        {/* 총 갯수 부 */}
+                        <section className="bg-gray-100 p-4 rounded-md mt-4 flex items-center space-x-8">
+                            <div>
+                                출처 총: <span className="text-red-500">{references.length}</span> 건
+                            </div>
+                            <span className="border-l border-black h-4"></span>
+                            <div>
+                                나이스비즈맵: <span className="text-red-500">{detailCategory.length}</span> 건
+                            </div>
+                            <span className="border-l border-black h-4"></span>
+                            <div>
+                                한국표준산업분류코드: <span className="text-red-500">준비중</span> 건
+                            </div>
+                            <span className="border-l border-black h-4"></span>
+                            <div>
+                                상권정보시스템: <span className="text-red-500"></span> 건
+                            </div>
+                        </section>
+
+
+                        {/* 검색 갯수 부 */}
+                        <section className="bg-white px-4 pb-4 pt-4 flex justify-between items-center">
+                            <div>
+                                검색결과: <span className="text-red-500">~~~</span> 개
+                            </div>
+                            <button
+                                className="px-4 py-2 bg-white text-black rounded border"
+                            >
+                                엑셀 다운로드
+                            </button>
+                        </section>
+
+                        
                     </section>
                 </main>
             </div>
