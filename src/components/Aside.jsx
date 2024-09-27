4f8a972367c1e17f6a236e3fd16023506d5fc3a8
DoorWinBell <msj102525@gmail.com>
2024-09-27 11:07:39 +0900

    Aside local주소로 변경 - 문승종 0927

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -18,7 +18,7 @@ const Aside = () => {
     return (
         <div className="px-10">
             <aside className='w-72'>
-                {/* <nav>
+                <nav>
                     <ul className='border'>
                         <li
                             className="text-xl p-2 flex justify-between items-center cursor-pointer leading-10"
@@ -77,8 +77,7 @@ const Aside = () => {
                                 <Link to="/locStore">매장정보</Link>
                             </li>
                             <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                
-                                <a href="http://192.168.0.240:3000/rising">뜨는 업종</a>
+                                <Link to="/rising">뜨는 업종</Link>
                             </li>
                             <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
                                 <Link to="/location/context">지역 상황정보(날씨, 환경, 일출몰)</Link>
@@ -115,104 +114,8 @@ const Aside = () => {
                             </li>
                         </div>
                     </ul>
-                </nav> */}
-                <nav>
-                    <ul className='border'>
-                        <li
-                            className="text-xl p-2 flex justify-between items-center cursor-pointer leading-10"
-                            onClick={() => toggleSection('section1')}
-                        >
-                            <div className="flex items-center">
-                                <div className="w-7 h-7">
-                                    <img className='block w-full h-auto' src={require("../assets/aside/aside_common_img.png")} alt="user-img" />
-                                </div>
-                                <p className='ml-2'>Common Table</p>
-                            </div>
-                            <p className='text-4xl'>
-                                {isOpen.section1 ? '-' : '+'}
-                            </p>
-                        </li>
-                        <div
-                            className={`transition-[max-height] duration-300 ease-in-out overflow-hidden ${isOpen.section1 ? 'max-h-[500px]' : 'max-h-0'}`}
-                        >
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.239:3000/city">Address Table</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.240:3000/category">업종분류</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <Link to="/">기타</Link>
-                            </li>
-                        </div>
-                    </ul>
-
-                    <ul className='border'>
-                        <li
-                            className="text-xl p-2 flex justify-between items-center cursor-pointer leading-10"
-                            onClick={() => toggleSection('section2')}
-                        >
-                            <div className="flex items-center">
-                                <div className="w-7 h-7">
-                                    <img className='block w-full h-auto' src={require("../assets/aside/aside_contents_img.png")} alt="info-img" />
-                                </div>
-                                <p className='ml-2'>Contents Table</p>
-                            </div>
-                            <p className='text-4xl'>
-                                {isOpen.section2 ? '-' : '+'}
-                            </p>
-                        </li>
-                        <div
-                            className={`transition-[max-height] duration-300 ease-in-out overflow-hidden ${isOpen.section2 ? 'max-h-[500px]' : 'max-h-0'}`}
-                        >
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.240:3000/commercial2">상권분석</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.239:3000/locInfo">입지정보</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.239:3000/locStore">매장정보</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.240:3000/rising">뜨는 업종</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.240:3000/location/context">지역 상황정보(날씨, 환경, 일출몰)</a>
-                            </li>
-                        </div>
-                    </ul>
-
-                    <ul className='border'>
-                        <li
-                            className="text-xl p-2 flex justify-between items-center cursor-pointer leading-10"
-                            onClick={() => toggleSection('section3')}
-                        >
-                            <div className="flex items-center">
-                                <div className="w-7 h-7">
-                                    <img className='block w-full h-auto' src={require("../assets/aside/aside_demographic_img.png")} alt="users-img" />
-                                </div>
-                                <p className='ml-2'>Demographic Table</p>
-                            </div>
-                            <p className='text-4xl'>
-                                {isOpen.section3 ? '-' : '+'}
-                            </p>
-                        </li>
-                        <div
-                            className={`transition-[max-height] duration-300 ease-in-out overflow-hidden ${isOpen.section3 ? 'max-h-[500px]' : 'max-h-0'}`}
-                        >
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <a href="http://192.168.0.239:3000/population">전국 인구 정보</a>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <Link to="/">점주 성향정보(준비중)</Link>
-                            </li>
-                            <li className='p-4 hover:bg-gray-200 text-slate-700 text-lg'>
-                                <Link to="/">방문객 성향정보(준비중)</Link>
-                            </li>
-                        </div>
-                    </ul>
                 </nav>
+
             </aside>
         </div>
     );
